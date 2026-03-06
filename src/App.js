bbad80bdc610f4a9bcedc535dc41533f7ab6628f
usolanki0312 <usolanki@cloudstrytech.com>
2026-03-06 10:32:41 +0530

    pdf generating for the selected

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 // src/App.js
-import React from 'react';
-import Navbar from './components/Navbar';
-import EventTable from './components/EventTable';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import UpdateEvent from './components/Update';
-import Eventcsr from './components/Event_CSR';
-import HomeData from './components/HomeData';
-import Auth from './components/Auth';
+import Navbar from "./components/Navbar";
+import EventTable from "./components/EventTable";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import UpdateEvent from "./components/Update";
+import Eventcsr from "./components/Event_CSR";
+import HomeData from "./components/HomeData";
 
 function App() {
   return (
@@ -14,10 +12,10 @@ function App() {
       <Navbar />
       <Routes>
         {/* <Route path='/' element={<Auth />}></Route>  */}
-        <Route path='/' element={<EventTable />}></Route>
-        <Route path='/update/:id' element={<UpdateEvent />}></Route>
-        <Route path='/event' element={<Eventcsr/>}/>
-        <Route path='/header' element={<HomeData />}></Route>
+        <Route path="/" element={<EventTable />}></Route>
+        <Route path="/update/:id" element={<UpdateEvent />}></Route>
+        <Route path="/event" element={<Eventcsr />} />
+        <Route path="/header" element={<HomeData />}></Route>
       </Routes>
     </Router>
   );
